5bb7c0a92ef6516048104d217692f3b36a5e693a
Mr.Moon <MrMoonKr@gmail.com>
2024-11-15 19:06:31 +0900

    VS Code 디버깅시 현재 경로와 exec-maven-plugin 사용시 현재 경로 설정 수정 ...

--- chapter-03/src/main/java/org/lwjglb/engine/Utils.java
+++ chapter-03/src/main/java/org/lwjglb/engine/Utils.java
@@ -1,5 +1,6 @@
 package org.lwjglb.engine;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
@@ -12,6 +13,20 @@ public class Utils {
     public static String readFile(String filePath) {
         String str;
         try {
+
+            //String basePath = new File("").getAbsolutePath() ;
+            //String basePath = Utils.class.getProtectionDomain().getCodeSource().getLocation().getPath() ;
+            //String fullPath = basePath + File.separator + filePath ;
+
+            Path path = Paths.get( "" ) ;
+            Path path1 = Paths.get( filePath ) ;
+            String basePath = path.toAbsolutePath().toString() ;
+            String basePath1 = path1.toAbsolutePath().toString() ;
+            String fullPath = basePath + File.separator + filePath ;
+            File file = new File( fullPath ) ;
+            //byte[] bytes = Files.readAllBytes( path ) ;
+            byte[] bytes = Files.readAllBytes( Paths.get( fullPath  )) ;
+            String data = new String( bytes ) ;
             str = new String(Files.readAllBytes(Paths.get(filePath)));
         } catch (IOException excp) {
             throw new RuntimeException("Error reading file [" + filePath + "]", excp);
